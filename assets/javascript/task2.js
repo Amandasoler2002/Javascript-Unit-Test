9db7fa804ade068cf1750353c6c00fc801e0f37f
Soler <20699@vtsd.com>
2020-01-07 11:38:55 -0500

    Update #2

--- assets/javascript/task2.js
+++ assets/javascript/task2.js
@@ -1,1 +1,13 @@
 /* Put code for the task below */
+function changeColor(){
+  document.getElementById("Table").style.backgroundColor = "red";
+}
+
+function Blue(){
+  document.getElementById("Table").style.backgroundColor = "blue";
+}
+
+function Green(){
+  document.getElementById("Table").style.backgroundColor = "Green";
+
+}
