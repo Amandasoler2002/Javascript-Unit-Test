f4209cb70f0e27d431117d549f5ae6e6d18d5d62
Soler <20699@vtsd.com>
2020-01-08 11:35:48 -0500

    Update Task 1

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,2 +1,7 @@
 /* Put code for the task below */
-function imageTask1
+function buttonTask1(){
+  document.getElementById('buttonTask1').style.display="inline";
+}
+function disappear(){
+  document.getElementById('imageTask1').style.display="none";
+}
